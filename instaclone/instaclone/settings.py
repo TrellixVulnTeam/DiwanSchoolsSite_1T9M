f08aa40919eedd36f4ec4b454cf02941903fb94c
Brian Scheuermann <brian@Brians-Air.wv.cc.cmu.edu>
2018-05-11 05:19:14 -0400

    better now

--- instaclone/instaclone/settings.py
+++ instaclone/instaclone/settings.py
@@ -135,9 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
     os.path.join(PROJECT_ROOT, 'instaclone/static'),
-    '/Users/brian/instaclone-3/instaclone/static/'
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
